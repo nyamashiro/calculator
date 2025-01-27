b6cd618d7ec5c4bfee3178bd13418bf293a229b1
Nathan Y <193931003+nyamashiro@users.noreply.github.com>
2025-01-27 11:37:58 -0500

    create basic operation functions

---
+++ script.js
@@ -0,0 +1,19 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+function add (num1, num2) {
+  return num1 + num2
+}
+
+function subtract (num1, num2) {
+  return num1 - num2
+}
+
+function multiply (num1, num2) {
+  return num1 * num2
+}
+
+function divide (num1, num2) {
+  return num1 / num2
+}
