460ddbbded2f4766ed3d238a05350dccc9a3f2cb
Nathan Y <193931003+nyamashiro@users.noreply.github.com>
2025-01-27 18:29:37 -0500

    allow basic calculator functionality

--- script.js
+++ script.js
@@ -1,84 +1,177 @@
-let displayNumbers = "";
-let firstNumber = "";
-let secondNumber = "";
-let operator;
+let calculatorObj = {
+  firstNumber: "",
+  secondNumber: "",
+  operator: "",
+  fullExpression: false,
+  displayNumbers: "",
+}
 
-const number = document.querySelector(".buttons-container");
+const numberButtons = document.querySelector(".number-buttons-container");
+const clearButton = document.querySelector("#clear")
+const operatorButtons = document.querySelector(".operator-buttons-container")
 const display = document.querySelector(".display-container")
 const numberOnDisplay = document.createElement("div");
 
 function displayFirstNumber(num) {
-  firstNumber += `${num}`
-  numberOnDisplay.textContent = firstNumber;
-  display.appendChild(numberOnDisplay)
-  console.log(firstNumber)
+  calculatorObj.firstNumber += `${num}`;
+  calculatorObj.displayNumbers = `${calculatorObj.firstNumber} ${calculatorObj.operator} ${calculatorObj.secondNumber}`;
+  numberOnDisplay.textContent = calculatorObj.displayNumbers;
+  display.appendChild(numberOnDisplay);
+  console.log(calculatorObj);
+}
+
+function displaySecondNumber(num) {
+  calculatorObj.secondNumber += `${num}`;
+  calculatorObj.displayNumbers = `${calculatorObj.firstNumber} ${calculatorObj.operator} ${calculatorObj.secondNumber}`;
+  numberOnDisplay.textContent = calculatorObj.displayNumbers;
+  display.appendChild(numberOnDisplay);
+  console.log(calculatorObj);
+}
+
+function displayOperator(op) {
+  calculatorObj.operator = `${op}`;
+  calculatorObj.displayNumbers = `${calculatorObj.firstNumber} ${calculatorObj.operator} ${calculatorObj.secondNumber}`;
+  numberOnDisplay.textContent = calculatorObj.displayNumbers;
+  display.appendChild(numberOnDisplay);
+  console.log(calculatorObj);
+}
+
+function clearSettings() {
+  calculatorObj.firstNumber = "";
+  calculatorObj.secondNumber = "";
+  calculatorObj.operator = "";
+  calculatorObj.fullExpression = false;
 }
 
 function clearDisplay() {
-  firstNumber = "";
-  numberOnDisplay.textContent = firstNumber;
-  display.appendChild(numberOnDisplay)
-  console.log(firstNumber)
+  clearSettings()
+  calculatorObj.displayNumbers = "";
+  numberOnDisplay.textContent = calculatorObj.displayNumbers;
+  display.appendChild(numberOnDisplay);
 }
 
-// function displayOperator(op) {
-//   operator = `${op}`;
-//   numberOnDisplay.textContent = operator;
-//   display.appendChild(numberOnDisplay);
-//   console.log(operator)
-// }
+clearButton.addEventListener("click", () => {
+  clearDisplay();
+})
 
-number.addEventListener("click", (e) => {
+operatorButtons.addEventListener("click", (e) => {
   let target = e.target;
+    switch (target.id) {
+      case "+":
+        displayOperator(target.id);
+        break;
 
-  switch (target.id) {
-    case "9":
-      displayFirstNumber(target.id)
-      break;
+      case "-":
+        displayOperator(target.id);
+        break;
 
-    case "8":
-      displayFirstNumber(target.id)
-      break;
+      case "*":
+        displayOperator(target.id);
+        break;
 
-    case "7":
-      displayFirstNumber(target.id)
-      break;
+      case "/":
+        displayOperator(target.id);
+        break;
 
-    case "6":
-      displayFirstNumber(target.id)
-      break;
+      case "=":
+        operate(calculatorObj.firstNumber, calculatorObj.secondNumber, calculatorObj.operator);
+        clearSettings();
+        calculatorObj.displayNumbers = `${calculatorObj.firstNumber} ${calculatorObj.operator} ${calculatorObj.secondNumber}`
+        break;
+    }
+})
 
-    case "5":
-      displayFirstNumber(target.id)
-      break;
+numberButtons.addEventListener("click", (e) => {
+  let target = e.target;
+  if (calculatorObj.operator) {
+    switch (target.id) {
+      case "9":
+        displaySecondNumber(target.id)
+        break;
 
-    case "4":
-      displayFirstNumber(target.id)
-      break;
+      case "8":
+        displaySecondNumber(target.id)
+        break;
 
-    case "3":
-      displayFirstNumber(target.id)
-      break;
+      case "7":
+        displaySecondNumber(target.id)
+        break;
 
-    case "2":
-      displayFirstNumber(target.id)
-      break;
+      case "6":
+        displaySecondNumber(target.id)
+        break;
 
-    case "1":
-      displayFirstNumber(target.id)
-      break;
+      case "5":
+        displaySecondNumber(target.id)
+        break;
 
-    case "0":
-      displayFirstNumber(target.id)
-      break;
+      case "4":
+        displaySecondNumber(target.id)
+        break;
 
-    case "clear":
-      clearDisplay();
+      case "3":
+        displaySecondNumber(target.id)
+        break;
+
+      case "2":
+        displaySecondNumber(target.id)
+        break;
+
+      case "1":
+        displaySecondNumber(target.id)
+        break;
+
+      case "0":
+        displaySecondNumber(target.id)
+        break;
+    }
+  } else {
+    switch (target.id) {
+      case "9":
+        displayFirstNumber(target.id)
+        break;
+
+      case "8":
+        displayFirstNumber(target.id)
+        break;
+
+      case "7":
+        displayFirstNumber(target.id)
+        break;
+
+      case "6":
+        displayFirstNumber(target.id)
+        break;
+
+      case "5":
+        displayFirstNumber(target.id)
+        break;
+
+      case "4":
+        displayFirstNumber(target.id)
+        break;
+
+      case "3":
+        displayFirstNumber(target.id)
+        break;
+
+      case "2":
+        displayFirstNumber(target.id)
+        break;
+
+      case "1":
+        displayFirstNumber(target.id)
+        break;
+
+      case "0":
+        displayFirstNumber(target.id)
+        break;
+    }
   }
 })
 
 function add(firstNumber, secondNumber) {
-  return firstNumber + secondNumber
+  return parseInt(firstNumber) + parseInt(secondNumber)
 }
 
 function subtract(firstNumber, secondNumber) {
@@ -96,15 +189,20 @@ function divide(firstNumber, secondNumber) {
 function operate(firstNumber, secondNumber, operator) {
   switch (operator) {
     case "+":
-      return add(firstNumber, secondNumber);
+      numberOnDisplay.textContent = add(firstNumber, secondNumber);
+      break;
 
     case "-":
-      return subtract(firstNumber, secondNumber);
+      numberOnDisplay.textContent = subtract(firstNumber, secondNumber);
+      break;
 
     case "*":
-      return multiply(firstNumber, secondNumber);
+      numberOnDisplay.textContent = multiply(firstNumber, secondNumber);
+      break;
 
     case "/":
-      return divide(firstNumber, secondNumber);
+      numberOnDisplay.textContent = divide(firstNumber, secondNumber);
+      break;
   }
+  display.appendChild(numberOnDisplay);
 }
